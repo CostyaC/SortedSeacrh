c5992df4e7a690b4880f52bda3991adfb5306656
CostyaC <114303055+CostyaC@users.noreply.github.com>
2022-10-18 23:22:51 +0200

    Add files via upload

---
+++ SortedSearch.java
@@ -0,0 +1,29 @@
+package SortedSearch;
+
+public class SortedSearch {
+
+	
+	
+public int SortedLinearSearch(int A[], int n, int data){
+	
+for(int i = 0; i < n; i++){
+	
+if(A[i] == data)
+	
+	return i;
+
+else if(A[i] > data)
+	
+	return -1;
+	
+	
+}
+	
+	return -1;
+	
+}
+	
+	
+	
+	
+}
---
+++ SortedTest.java
@@ -0,0 +1,15 @@
+package SortedSearch;
+
+public class SortedTest {
+
+	public static void main(String[] args) {
+		
+SortedSearch SortedTest = new SortedSearch();
+
+int[] A ={1,2,3,4,5,6,7,8,9,10};	
+
+
+System.out.println(SortedTest.SortedLinearSearch(A, 10, 9));
+	
+	}
+}
